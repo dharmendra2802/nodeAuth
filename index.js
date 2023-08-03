a13b42697655070bc90a946750425dc8f6b83a5f
dharmendra2802 <DHARMENDRABHADAURIA77@gmail.com>
2023-08-03 11:24:27 +0530

    env file added

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+
 const express = require('express');
-const expresslayout = require('express-ejs-layouts')
+const expresslayout = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 
@@ -16,16 +20,12 @@ const passportFacebook = require('./config/passportFacebook');
 const flash = require('connect-flash');
 const flashMiddle = require('./config/flash');
 
-//env 
-const env = require('./config/environment');
-
 const path = require('path');
 const PORT = 5000;
 
-
 const app = express();
 // setting up static file
-app.use(express.static(path.join(__dirname , env.asset_path)));
+app.use(express.static(path.join(__dirname , process.env.asset_path)));
 // multer
 app.use('/uploads' , express.static(__dirname+'/uploads'));
 
@@ -35,32 +35,31 @@ app.set('views',path.join(__dirname,'/views'));
 
 // template setup
 app.use(expresslayout);
-// so all script/style will remain at same place defined in template
+// so all script/style will remain at the same place defined in the template
 app.set('layout extractStyles' , true);
 app.set('layout extractScripts' , true);
 
 // logger
-app.use(morgan(env.morgan.mode,env.morgan.options))
+app.use(morgan('combined')); // or any other format you prefer
+
 // request conversion
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // passport / session setup
 app.use(session({
-    name: env.session_name,
-    secret: env.session_key,
-    resave:false,
-    saveUninitialized:false,
-    cookie:{maxAge:100*60*100},
+    name: process.env.session_name,
+    secret: process.env.session_key,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 100 * 60 * 100 },
     store: MongoStore.create({
-        mongoUrl:`mongodb://127.0.0.1:27017/${env.db_name}`,
-        collection:'session',
-        autoRemove:'interval',
+        mongoUrl: `mongodb://127.0.0.1:27017/${process.env.db_name}`,
+        collection: 'session',
+        autoRemove: 'interval',
         autoRemoveInterval: 1,
-    },function(err){
-        console.log('error in Session Setup');
     }),
-}))
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -69,18 +68,12 @@ app.use(passport.setAuthenticatedUser);
 app.use(flash());
 app.use(flashMiddle.setFlash);
 
-
-// gulp
-// const gulp = require('./gulp')
 // setting up route
 app.use('/',require('./routes/index'));
-// 
 
+app.listen(PORT, function(err) {
+    if (err)
+        console.log("Error in starting server - " + err);
 
-app.listen(PORT,function(err)
-{
-    if(err)
-        console.log("Error in starting server - "+err);
-    
     console.log("Server Started Successfully");
-})
+});
