3d597b8920c1cb77d86fe08ddfa58f0875f02c26
dharmendra bhadauria <73592163+dharmendra2802@users.noreply.github.com>
2023-08-04 02:21:06 +0530

    Update mongoose.js
    mongo error

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ main().catch(err=>{console.log("Error in connecting to mongoDB - "+err)});
 
 async function main()
 {
-    await mongoose.connect(`mongodb://127.0.0.1:27017/${process.env.db_name}`);
+    await mongoose.connect(`mongodb://localhost:27017/${process.env.db_name}`);
     const db = mongoose.connection;
 
     console.log("Connected to mongoDB");
